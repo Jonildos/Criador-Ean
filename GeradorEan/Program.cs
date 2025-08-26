a7c06fc5854103d224ca1bc41ae94391bfd85580
Jonas Soares <jonassoares2209@gmail.com>
2025-08-26 08:47:59 -0300

    Fix

--- GeradorEan/Program.cs
+++ GeradorEan/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using GeradorEan.Services;
 
 class Program
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("--- Gerador de Código EAN-13 ---");
-        Console.Write("Digite o número de códigos EAN desejados: ");
-        string EanCode = Console.ReadLine();
-        int quantidade = int.Parse(EanCode);
+        int quantidade;
 
-        Console.WriteLine("Gerando " + EanCode + " Códigos Ean.");
+        while(true)
+        {
+            Console.Write("Digite a quantidade de códigos EAN-13 a serem gerados: ");
+            if (int.TryParse(Console.ReadLine(), out quantidade) && quantidade > 0)
+            {
+                break;
+            }
+            Console.WriteLine("Por favor, insira um número inteiro positivo.");
+        }
+
+
     }
 }
